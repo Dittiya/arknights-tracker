6c287477c0884bff842772aeacdb3575822d4b75
Aditiya <aditiyaart23@gmail.com>
2024-08-31 17:37:23 +1200

    added smaller opencv

--- src/History/History.tsx
+++ src/History/History.tsx
@@ -1,13 +1,22 @@
-// why 'Module' instead of 'cv'? https://github.com/opencv/opencv/issues/21580
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// declare let Module: any;
+// why 'Module' instead of 'cv'? refer to https://github.com/opencv/opencv/issues/21580
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare let Module: any;
 
 import { useState } from "react"
 import Canvas from "./Canvas"
 import Uploader from "./Uploader"
 
-function History() {  
+function History() {
   const [imageUrl, setImageUrl] = useState([] as string[])
+  const cv = Module
+
+  /* OpenCV WASM runtime refer to the following link
+   * https://stackoverflow.com/questions/56671436/cv-mat-is-not-a-constructor-opencv 
+  */
+  cv.onRuntimeInitialized = () => {
+    console.log("test")
+    console.log(cv.ACCESS_FAST)
+  }
 
   return (
     <>
